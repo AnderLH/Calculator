237f824750f94df2c91a648022b3583f1729f51a
AnderLH <ander.lopezdevallejohi@elorrieta-errekamari.com>
2023-09-25 15:33:25 +0200

    instance state changed

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -8,18 +8,18 @@ import android.widget.TextView
 import com.example.calculatorapp.Operations.doOperation
 
 class MainActivity : AppCompatActivity() {
-
+    private var operation:String? = "0"
     private var result:String? = "0"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var operation:String = ""
+
         val text: TextView = findViewById<TextView>(R.id.textViewNumbers)
 
         val buttonN1: Button = findViewById<Button>(R.id.buttonNum1)
-        val buttonN2: Button = findViewById<Button>(R.id.buttonNum2)
+        val buttonN2: Button = findViewById<Button>(R.id.buttonNum2)    
         val buttonN3: Button = findViewById<Button>(R.id.buttonNum3)
         val buttonN4: Button = findViewById<Button>(R.id.buttonNum4)
         val buttonN5: Button = findViewById<Button>(R.id.buttonNum5)
@@ -33,121 +33,100 @@ class MainActivity : AppCompatActivity() {
         val buttonMin: Button = findViewById<Button>(R.id.buttonMinus)
         val buttonEq: Button = findViewById<Button>(R.id.buttonEqual)
 
-        //clase anonima
-        /*Button button = (Button) findViewById(R.id.buttonNum0)
-        button.setOnClickListener(new View.OnClickListener()){
-            @override
-            public void onClick(View v) {
-
-            }
-        }*/
-
         buttonN1.setOnClickListener {
             operation += "1"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN2.setOnClickListener {
             operation += "2"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN3.setOnClickListener {
             operation += "3"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN4.setOnClickListener {
             operation += "4"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN5.setOnClickListener {
             operation += "5"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN6.setOnClickListener {
             operation += "6"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN7.setOnClickListener {
             operation += "7"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN8.setOnClickListener {
             operation += "8"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN9.setOnClickListener {
             operation += "9"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonN0.setOnClickListener {
             operation += "0"
-            result=operation
-            text.text = result.toString()
+            text.text = operation.toString()
         }
 
         buttonPls.setOnClickListener {
-            if (operation.isNotEmpty()) {
-                operation = "$operation+"
-                text.text = operation.toString()
-            }
+            operation = "$operation+"
+            text.text = operation.toString()
+
         }
         buttonMin.setOnClickListener {
             operation = "$operation-"
             text.text = operation.toString()
         }
         buttonEq.setOnClickListener {
-            if (operation.isNotEmpty()) {
-                result = doOperation(operation)
-                if (result.equals(null)){
-                    result = "Error"
-                    text.text = result
-                }else{
-                    text.text = result.toString()
-                }
-                operation = ""
+            result = operation.toString()
+            result = doOperation(result!!)
+            if (result.equals(null)){
+                result = "Error"
+                text.text = result
+            }else{
+                text.text = result.toString()
             }
+            operation = ""
         }
 
     }
 
    override fun onResume(){
         super.onResume()
-        Log.d("Debug","onResume")
         val text: TextView = findViewById(R.id.textViewNumbers)
-        text.text = result.toString()
+        text.text = operation.toString()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         // Save the user's current game state.
         outState?.run {
-            putString("contador", result)
+            putString("contador", operation)
         }
         // Always call the superclass so it can save the view hierarchy.
         super.onSaveInstanceState(outState)
     }
+
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         // Always call the superclass so it can restore the view hierarchy.
         super.onRestoreInstanceState(savedInstanceState)
         // Restore state members from saved instance.
         savedInstanceState?.run {
-            result = savedInstanceState.getString("contador").toString()
+            operation = savedInstanceState.getString("contador").toString()
         }
     }
 
